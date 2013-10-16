bbdcbcc2876ab24bd1a98615fb38e6d3bdc7d43d
Drew Gottlieb <dag10@rancor.csh.rit.edu>
2013-10-16 18:55:28 -0400

    Added port and host to config, and added 'requests' to setup.

--- website/run.py
+++ website/run.py
@@ -1,5 +1,13 @@
 #!flask/bin/python
-
+import yaml
 from app import app
-app.run(debug=True)
+
+try:
+  with open('app.yaml', 'r') as f:
+    config = yaml.load(f)
+except:
+  print 'Missing/invalid config (app.yaml)'
+  quit()
+
+app.run(debug=True, port=config['port'], host=config['host'])
 
--- website/setup.sh
+++ website/setup.sh
@@ -6,4 +6,5 @@ flask/bin/pip install flask==0.9
 flask/bin/pip install --upgrade google-api-python-client
 flask/bin/pip install pytz
 flask/bin/pip install PyYAML
+flask/bin/pip install requests
 
