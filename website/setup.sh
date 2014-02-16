ec30e4509aca0244cde212e98030dd2a1c580703
Drew Gottlieb <gottlieb.drew@gmail.com>
2014-02-16 14:19:53 -0500

    Setup.sh explicitly uses python2.7 now.

--- website/setup.sh
+++ website/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install Flask
-python virtualenv.py flask
+python2.7 virtualenv.py flask
 flask/bin/pip install flask==0.9
 flask/bin/pip install --upgrade google-api-python-client
 flask/bin/pip install pytz
