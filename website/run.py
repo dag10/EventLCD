9989a97b3a5cf905c7383009c46dda4130969318
Drew Gottlieb <gottlieb.drew@gmail.com>
2014-04-06 13:35:44 -0400

    Wrap Flask with Tornado for hopefully improved stability.

--- website/run.py
+++ website/run.py
@@ -1,6 +1,9 @@
 #!flask/bin/python
 import yaml
 from app import app
+from tornado.wsgi import WSGIContainer
+from tornado.ioloop import IOLoop
+from tornado.web import FallbackHandler, Application
 
 try:
   with open('app.yaml', 'r') as f:
@@ -9,5 +12,11 @@ except:
   print 'Missing/invalid config (app.yaml)'
   quit()
 
-app.run(debug=True, port=config['port'], host=config['host'])
+#app.run(debug=True, port=config['port'], host=config['host'])
+
+application = Application([
+  (r'.*', FallbackHandler, dict(fallback=WSGIContainer(app)))])
+
+application.listen(config['port'])
+IOLoop.instance().start()
 
--- website/setup.sh
+++ website/setup.sh
@@ -2,7 +2,8 @@
 
 # Install Flask
 python2.7 virtualenv.py flask
-flask/bin/pip install flask==0.9
+flask/bin/pip install flask==0.10
+flask/bin/pip install tornado
 flask/bin/pip install --upgrade google-api-python-client
 flask/bin/pip install pytz
 flask/bin/pip install PyYAML
